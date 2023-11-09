4a91df24c3b6ea51a0712fb32517ff4bcdcae416
mavie <114839556+RayEsther@users.noreply.github.com>
2023-11-09 14:37:20 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+// Defina a função.
+
+  // Obtenha o nome do usuário da input box e armazene dentro de uma variável
+
+  // Então, armazenamos o valor da variável userName no local storage com a palavra-chave userName.
+  
+  // Em seguida, redirecionamos para kwitterRoom.html.
+
+
+
